11f71c3ed804ef50b56ebddb63916a82520e984f
liuliuliu <1240962938@qq.com>
2018-04-01 20:12:11 +0800

    sortCompare

--- src/main/java/com/algs/test/c2/SortCompare.java
+++ src/main/java/com/algs/test/c2/SortCompare.java
@@ -1,5 +1,40 @@
 package com.algs.test.c2;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
 public class SortCompare {
-    
+   public static double time(String alg,Double[] a){
+       Stopwatch timer = new Stopwatch();
+       if (alg.equals("insertion")){
+           MyInsertion.sort(a);
+       }else if (alg.equals("insertionX")){
+           MyInsertion.sortX(a);
+       }else if (alg.equals("selection")){
+           MySelection.sort(a);
+       }else if (alg.equals("shell")){
+
+       }
+
+       return timer.elapsedTime();
+   }
+
+   public static double timeRandonInput(String alg,int N,int T){
+       double total = 0.0;
+       Double[] a= new Double[N];
+       for (int i = 0; i < T; i++) {
+           for (int j = 0; j < N ; j++) {
+               a[j] = StdRandom.uniform();
+           }
+           total += time(alg,a);
+       }
+
+       return total;
+   }
+
+    public static void main(String[] args) {
+        double t1 = timeRandonInput("selection",1000,100);
+        double t2 = timeRandonInput("insertionX",1000,100);
+        System.out.println(t1/t2);
+    }
 }
